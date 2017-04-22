3a9f2395897d86eba8c86a29a1fb78dae11af40b
Gil Cesar Faria <gilcesarf@gmail.com>
2017-04-21 23:23:33 -0300

    - Added a few new utility methods

--- src/main/java/io/github/gilcesarf/commons/configuration2/zookeeper/json/ZookeeperConfigurationJsonUtil.java
+++ src/main/java/io/github/gilcesarf/commons/configuration2/zookeeper/json/ZookeeperConfigurationJsonUtil.java
@@ -9,8 +9,15 @@ import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 
 public final class ZookeeperConfigurationJsonUtil {
+    /**
+     * Cannot be instantiated
+     */
+    private ZookeeperConfigurationJsonUtil() {
+    }
+
     public static String toJsonString(Object obj) throws JsonGenerationException, JsonMappingException, IOException {
         ObjectMapper mapper = new ObjectMapper();
         mapper.configure(SerializationFeature.INDENT_OUTPUT, false);
@@ -21,10 +28,24 @@ public final class ZookeeperConfigurationJsonUtil {
         return new String(baos.toByteArray());
     }
 
-    public static <T> T fromJsonString(String json, Class<T> c) throws JsonParseException, JsonMappingException, IOException {
+    public static <T> T fromJsonString(String json, Class<T> c)
+            throws JsonParseException, JsonMappingException, IOException {
         ObjectMapper mapper = new ObjectMapper();
         T obj = (T) mapper.readValue(json, c);
         return obj;
     }
 
+    public static ObjectNode fromPojoToObjectNode(Object pojo)
+            throws JsonParseException, JsonMappingException, IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        ObjectNode obj = (ObjectNode) mapper.valueToTree(pojo);
+        return obj;
+    }
+
+    public static <T> T fromObjectNodeToPojo(ObjectNode node, Class<T> c)
+            throws JsonParseException, JsonMappingException, IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        T obj = (T) mapper.treeToValue(node, c);
+        return obj;
+    }
 }
